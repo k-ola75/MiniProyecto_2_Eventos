413b0a1e20c81d77842d4bb5458898b286e120b0
k-ola75 <98629226+k-ola75@users.noreply.github.com>
2022-11-30 19:07:43 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,17 @@
+/*
+  Autores:
+    Juan Sebastian Oviedo Oviedo (2179238-2724)
+    Kirk Olaya Villamarín (2178638-2724)
+  E-mail:
+    Juan.sebastian.oviedo@orreounivalle.edu.co
+    kikr.olaya@correounivalle.edu.co
+  Fecha: 2022-11-30
+  Propósito: Académico
+  Versión actual: 2.0
+  Versión anterior: 1.0
+*/
+
+
 import gui.VentanaPrincipal;
 
 public class Main {
